7cb93ac168e15bdb4e1105e88a9df4228829adf5
tenopee <tenotenotenopee@gmail.com>
2021-05-19 22:36:21 +0900

    [add]vue-routerの再設定

--- src/router.js
+++ src/router.js
@@ -1,14 +1,17 @@
-import Vue from "vue";
-import Router from "vue-router";
+import Vue from 'vue'
+import Router from 'vue-router'
+import Settings from './templates/settings'
+import GameMemo from './templates/gameMemo'
 
-Vue.use(Router);
+Vue.use(Router)
+
 export default new Router({
-  mode: "history",
+  mode: 'history',
   routes: [
     {
       path: '/',
-      component: Index,
-      name: 'setting',
+      component: Settings,
+      name: 'settings',
     },
     {
       path: '/gameMemo',            
@@ -16,4 +19,4 @@ export default new Router({
       name: 'gameMemo',
     },
   ]
-});
+})
